67ab062bba282b92b7e20101b8efa4e0d8c8ca2b
Narayana Raj <48882141+narayanarajvj@users.noreply.github.com>
2020-08-23 20:44:02 +0530

    Add files via upload

---
+++ packages/configuration.py
@@ -0,0 +1,22 @@
+# base path to YOLO directory
+MODEL_PATH = "yolo-coco"
+
+# initialize minimum probability to filter weak detections
+MIN_CONF = 0.3
+
+# the threshold when applying non-maxima suppression
+NMS_THRESH = 0.3
+
+# boolean indicating if NVIDIA CUDA GPU should be used
+USE_GPU = False
+
+# define the minimum safe distance (in pixels) that two people can be from each other
+MIN_DISTANCE = 50
+
+
+
+
+
+
+
+
